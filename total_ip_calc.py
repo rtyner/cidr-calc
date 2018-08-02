d79042bed0955347b2fe41f1d0931f897ed3da1a
rtyner <rusty@rtyner.com>
2018-08-02 16:03:55 -0400

    have total IPs by network, need to add those for total

---
+++ total_ip_calc.py
@@ -0,0 +1,20 @@
+import sys
+import netaddr
+from netaddr import IPNetwork
+
+in_file = sys.argv[1]
+
+with open(in_file,'r') as i:
+    lines = i.readlines()
+    for line in lines:
+        ip = IPNetwork(line)
+        print(ip.size)
+        
+        
+# testing some shit here
+        #ip_size = []
+        #ip_size.append(ip.size)
+        #print(ip_size)
+        #sum(ip_size)
+       # ip_size = sum(ip_count)
+       # print(ip_size)
